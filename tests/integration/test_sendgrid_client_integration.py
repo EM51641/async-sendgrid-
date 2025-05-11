594c75e3dfc257ee0a43e3ebd610ffd007363211
Elyes <65829912+EM51641@users.noreply.github.com>
2025-05-11 22:35:16 +0200

    refactored tests to stop using classes so as to make them the more independent possible

--- tests/integration/test_sendgrid_client_integration.py
+++ tests/integration/test_sendgrid_client_integration.py
@@ -7,97 +7,90 @@ from async_sendgrid.exception import SessionClosedException
 from async_sendgrid.sendgrid import SendgridAPI
 
 
-class TestSendgridClient:
-
-    @pytest.fixture()
-    def client(self):
-        """Setup client"""
-        secret_key = os.environ["SENDGRID_API_KEY"]
-        impersonate_subuser = "John Smith"
-        endpoint = "http://localhost:3000/v3/mail/send"
-        client = SendgridAPI(
-            api_key=secret_key,
-            endpoint=endpoint,
-            impersonate_subuser=impersonate_subuser,
+@pytest.fixture
+def client():
+    """Setup client"""
+    secret_key = os.environ["SENDGRID_API_KEY"]
+    impersonate_subuser = "John Smith"
+    endpoint = "http://localhost:3000/v3/mail/send"
+    client = SendgridAPI(
+        api_key=secret_key,
+        endpoint=endpoint,
+        impersonate_subuser=impersonate_subuser,
         )
 
-        yield client
+    yield client
 
-        request("DELETE", url="http://localhost:3000/api/mails")
+    request("DELETE", url="http://localhost:3000/api/mails")
 
-    @pytest.fixture()
-    def email(self) -> Mail:
-        email = Mail(
-            from_email="johndoe@example.com",
-            to_emails="mahndoe@example.com",
-            subject="Example email",
-            plain_text_content="Hello World!",
-        )
-        return email
+@pytest.fixture
+def email() -> Mail:
+    email = Mail(
+        from_email="johndoe@example.com",
+        to_emails="mahndoe@example.com",
+        subject="Example email",
+        plain_text_content="Hello World!",
+    )
+    return email
 
-    @pytest.fixture
-    def messages_received(self):
+@pytest.fixture
+def messages_received():
         response = request("GET", url="http://localhost:3000/api/mails")
         return response.json()
 
-    @pytest.mark.asyncio
-    async def test_post_status(self, client: SendgridAPI, email: Mail) -> None:
-        """
-        Test the status code of the POST request.
+@pytest.mark.asyncio
+async def test_post_status(client: SendgridAPI, email: Mail) -> None:
+    """
+    Test the status code of the POST request.
 
-        Args:
-            email (Mail): The email object to be sent.
+    Args:
+        email (Mail): The email object to be sent.
+    Returns:
+        None
+    Raises:
+        AssertionError: If the response status code is not 202.
+    """
+    async with client:
+        response = await client.send(email)
 
-        Returns:
-            None
+    assert response.status_code == 202
 
-        Raises:
-            AssertionError: If the response status code is not 202.
-        """
-        async with client:
-            response = await client.send(email)
-
-        assert response.status_code == 202
-
-    @pytest.mark.asyncio
-    async def test_session_closed_exception(self, client: SendgridAPI, email: Mail) -> None:
-        """
-        Test that the SessionClosedException is raised when the session is closed.
-        """
-        async with client:
-
-            assert client.session
-            await client.session.aclose()
-            assert client.session.is_closed
-    
-            with pytest.raises(SessionClosedException):
-                await client.send(email)
-
-    @pytest.mark.asyncio
-    async def test_if_messages_sent_are_correct(self, client: SendgridAPI, email: Mail) -> None:
-        """
-        Test if the sent messages are valid.
-
-        Args:
-            email (Mail): The email message to be sent.
-
-        Returns:
-            None
-        """
-        async with client:
+@pytest.mark.asyncio
+async def test_session_closed_exception(client: SendgridAPI, email: Mail) -> None:
+    """
+    Test that the SessionClosedException is raised when the session is closed.
+    """
+    async with client:
+        assert client.session
+        await client.session.aclose()
+        assert client.session.is_closed
+        with pytest.raises(SessionClosedException):
             await client.send(email)
 
-        response = request("GET", url="http://localhost:3000/api/mails")
-        messages = response.json()
+@pytest.mark.asyncio
+async def test_if_messages_sent_are_correct(client: SendgridAPI, email: Mail) -> None:
+    """
+    Test if the sent messages are valid.
+    Args:
+        email (Mail): The email message to be sent.
+    Returns:
+        None
+    """
+    async with client:
+        await client.send(email)
 
-        assert len(messages) == 1
-        msg = messages[0]
+    response = request("GET", url="http://localhost:3000/api/mails")
+    messages = response.json()
 
-        assert msg["from"] == {"email": "johndoe@example.com"}
-        assert msg["subject"] == "Example email"
-        assert msg["personalizations"] == [
-            {"to": [{"email": "mahndoe@example.com"}]}
-        ]
-        assert msg["content"] == [
-            {"type": "text/plain", "value": "Hello World!"}
-        ]
+    assert len(messages) == 1
+
+    msg = messages[0]
+
+    assert msg["from"] == {"email": "johndoe@example.com"}
+    assert msg["subject"] == "Example email"
+    assert msg["personalizations"] == [
+        {"to": [{"email": "mahndoe@example.com"}]}
+    ]
+    assert msg["content"] == [
+        {"type": "text/plain", "value": "Hello World!"}
+    ]
--- tests/unit/test_sendgrid_client.py
+++ tests/unit/test_sendgrid_client.py
@@ -2,28 +2,25 @@ import pytest
 
 from async_sendgrid.sendgrid import SendgridAPI
 
+@pytest.fixture
+def client() -> SendgridAPI:
+    secret_key = "SECRET_KEY"
+    impersonate_subuser = "John Smith"
+    client = SendgridAPI(
+        api_key=secret_key, impersonate_subuser=impersonate_subuser
+    )
+    return client
 
-class TestAsyncClient:
-    @pytest.fixture(autouse=True)
-    def _setup(self) -> None:
-        secret_key = "SECRET_KEY"
-        impersonate_subuser = "John Smith"
-        self._service = SendgridAPI(
-            api_key=secret_key, impersonate_subuser=impersonate_subuser
-        )
-
-    def test_constructor(self) -> None:
-        """
-        Test constructor.
-        """
-        assert (
-            self._service.endpoint == "https://api.sendgrid.com/v3/mail/send"
-        )
-        assert self._service.api_key == "SECRET_KEY"
-        assert self._service.headers == {
-            "Authorization": "Bearer SECRET_KEY",
-            "User-Agent": "sendgrid-async;python",
-            "Accept": "*/*",
-            "Content-Type": "application/json",
-            "On-Behalf-Of": "John Smith",
-        }
+def test_constructor(client: SendgridAPI) -> None:
+    """
+    Test constructor.
+    """
+    assert client.endpoint == "https://api.sendgrid.com/v3/mail/send"
+    assert client.api_key == "SECRET_KEY"
+    assert client.headers == {
+        "Authorization": "Bearer SECRET_KEY",
+        "User-Agent": "sendgrid-async;python",
+        "Accept": "*/*",
+        "Content-Type": "application/json",
+        "On-Behalf-Of": "John Smith",
+    }
