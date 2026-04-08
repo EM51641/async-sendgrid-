de7dffad65f9fe3a6abe863ba7d0c2ec63255bac
Elyes <65829912+EM51641@users.noreply.github.com>
2026-04-08 13:58:29 +0100

    added a handler for development when the `__version__` is not available

--- async_sendgrid/__init__.py
+++ async_sendgrid/__init__.py
@@ -2,5 +2,11 @@ from importlib.metadata import version
 
 from .sendgrid import SendgridAPI  # noqa
 
-__version__ = version("sendgrid-async")
+try:
+    from importlib.metadata import version
+
+    __version__ = version("sendgrid-async")
+except Exception:
+    __version__ = "0.0.0-dev"
+
 __all__ = ["SendgridAPI"]
